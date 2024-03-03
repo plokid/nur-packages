bab38ada9c202ead6783abbef8c747a8c0a0a475
plokid <910576949@qq.com>
2024-03-03 21:47:05 +0800

    finish imewlconvter

--- pkgs/imewlconverter/default.nix
+++ pkgs/imewlconverter/default.nix
@@ -2,21 +2,25 @@
 
 buildDotnetModule rec {
   name = "imewlconverter";
-  version = "v3.1.0";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "studyzy";
     repo = "imewlconverter";
-    rev = version;
+    rev = "v$version";
     sha256="TSB0SmRoI79nlGlFm/+FQ8GymhCEkuNKX0NXHHw0u8s=";
   };
 
-  projectFile = "src/ImeWlConverterCmd.sln";
-
-  nugetDeps="" ;
+  projectFile = "src/ImeWlConverterCmd/ImeWlConverterCmd.csproj";
+  
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+  
+  nugetDeps = ./deps.nix;
 
   meta = with lib; {
     description = "”深蓝词库转换“ 一款开源免费的输入法词库转换程序 ";
+    homepage = "https://github.com/studyzy/imewlconverter";
     license = licenses.gpl3;
     platforms = platforms.all;
   };
