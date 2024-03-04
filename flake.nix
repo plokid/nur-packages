62becf74dde6e5970ca143043b7749fd9ab78295
plokid <910576949@qq.com>
2024-03-04 20:21:24 +0800

    add overlay support

--- flake.nix
+++ flake.nix
@@ -18,5 +18,6 @@
         pkgs = import nixpkgs { inherit system; };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      overlays.default = import ./overlay.nix;
     };
 }
