66620d7420153d05b59cff957d30a4ec3e28be6a
plokid <910576949@qq.com>
2024-03-06 13:06:29 +0800

    remove example-package

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   imewlconverter = pkgs.callPackage ./pkgs/imewlconverter { };
   # ...
 }
